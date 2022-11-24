6fca8844e9c3d5a7258f6eed0ff29adb9e55de38
dunghenry <trandungksnb6930082001>
2022-11-24 22:24:50 +0700

    refactor de1 and add docker

--- de1/src/connectdb.php
+++ de1/src/connectdb.php
@@ -1,10 +1,12 @@
 <?php
-$host = "localhost";
+$host = "mysql_db";
 $username = "root";
-$password = "";
+$password = "root";
 $database_name = "electric";
 $conn = new mysqli($host, $username, $password, $database_name);
 if (!$conn) {
     echo "Connected to MYSQL error";
     die(mysqli_error($conn));
 }
+
+?>
